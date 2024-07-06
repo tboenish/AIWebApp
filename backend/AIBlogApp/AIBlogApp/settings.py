adb78335f418ca8ca58feebacd1b73ab130b6a85
Thomas Boenish <tboenish@gmail.com>
2024-07-05 18:06:15 -0700

    Linked database
    I can now migrate data to my postgresql database.

--- backend/AIBlogApp/AIBlogApp/settings.py
+++ backend/AIBlogApp/AIBlogApp/settings.py
@@ -75,10 +75,24 @@ WSGI_APPLICATION = 'AIBlogApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'qoveryadmin',
+        'PASSWORD':'w4mHiNcWF43IcYDBijr4794rtwxJtV-f',
+        'HOST': 'z96a8a1b3-postgresql.c3u2m0u0grhv.us-east-2.rds.amazonaws.com',
+        'PORT':'5432',
+
     }
 }
 
